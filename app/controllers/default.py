f7c595fe5c67ad5a9e28f2291b8adfce63ea829b
leonardo.dutra <leonardo.dutra@exceedlabs.com>
2018-06-25 18:46:18 -0300

    --DONE: I organized the config files, create the storage with the defined model. TO DO: 1.Create the pages with a template; 2.Adjust the routes; 3. Create the forms;

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,15 @@
 from app import app
-
+from flask import request
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "Hello World"
+    return "<h1>Hello World</h1>"
+
+
+@app.route("/login")
+def login():
+    if request.method == 'POST':
+        return login()
+    else:
+        return ";*"
--- app/models/tables.py
+++ app/models/tables.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename_ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Columns(db.String(80), unique=True, nullabel=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
@@ -25,22 +25,23 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     owner = db.relationship("User", foreign_keys=user_id)
 
     def __init__(self, content, user_id):
         self.content = content
-        self.user_id - user_id
+        self.user_id = user_id
 
     def __repr__(self):
         return "<Post %r>" % self.id
 
-class Follow(db.model):
+class Follow(db.Model):
     __tablename__ = "followers"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     owner = db.relationship("User", foreign_keys=user_id)
+    follower = db.relationship("User", foreign_keys=follower_id)
